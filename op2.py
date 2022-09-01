60f33666d911ddab467cc003499517323a07103a
Sarika Rathore <rathor.sarika1808@gmail.com>
2022-09-01 19:53:34 +0530

    op1 op2 op3 op4.py

---
+++ op2.py
@@ -0,0 +1,41 @@
+
+print('ASSINGMENTOPERATORS')
+x=15#simple assignment
+y=x+15
+z=x+y
+a=x**2+y**2
+
+print(x,y,z,a)
+
+
+
+b=10
+b+=5 #updation assingment
+print(b)
+b-=2#updation assigment
+print(b)
+b*=3
+print(b)
+b/=2
+print(b)
+
+
+print('ASSINGMENTOPERATORS')
+x=20#simple assignment
+y=x+10
+z=x+y
+a=x**2+y**2
+
+print(x,y,z,a)
+
+
+
+b=10
+b+=10 #updation assingment
+print(b)
+b-=2#updation assigment
+print(b)
+b*=3
+print(b)
+b/=2
+print(b)
---
+++ op3.py
@@ -0,0 +1,26 @@
+a=100
+b=40
+print('COMPARISON operators')
+print(a==100)
+print(b==100)
+print(a==b+60)
+print(5==3)
+print(a!=20)
+print(a>b)
+print(a<b)
+print(a<=500)
+print(a>=500)
+
+
+a=200
+b=80
+print('COMPARISON operators')
+print(a==100)
+print(b==100)
+print(a==b+60)
+print(5==3)
+print(a!=20)
+print(a>b)
+print(a<b)
+print(a<=500)
+print(a>=500)
